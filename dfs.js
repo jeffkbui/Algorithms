beadf29783a82cdd07ef35ec1384eac23e514532
Jeffrey Bui <jeffkbui@gmail.com>
2019-12-05 15:45:14 -0800

    updated

---
+++ dfs.js
@@ -0,0 +1,19 @@
+class Node {
+    constructor(name) {
+      this.name = name;
+      this.children = [];
+    }
+  
+    addChild(name) {
+      this.children.push(new Node(name));
+      return this;
+    }
+  
+    depthFirstSearch(array) {
+      // Write your code here.
+    }
+  }
+  
+  // Do not edit the line below.
+  exports.Node = Node;
+  
