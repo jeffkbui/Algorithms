029619b2cdf8ae0c461d693204f23d0d079f8640
Jeffrey Bui <jeffkbui@gmail.com>
2020-01-09 10:06:11 -0800

    algos

--- dfs.js
+++ dfs.js
@@ -4,13 +4,14 @@ class Node {
     this.children = [];
   }
 
-  addChild(name) {
+  addChild(name) { 
     this.children.push(new Node(name));
     return this;
   }
 
   depthFirstSearch(array) {
     // Write your code here.
+    // push child to end
   }
 }
 
